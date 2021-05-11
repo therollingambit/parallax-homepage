a53f86ebd9a5e6b292ed2123ab5c29e2907d471a
therollingambit <therollingambit@gmail.com>
2021-05-11 17:35:24 +0800

    added img folder

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ timeline
     .to('.girl', 3, {y: -200}, '-=3')
     .fromTo('.bg1', 3, {y: -50}, {y: 0, duration: 3}, '-=3')
     .to('.content', 3, {top: '0%'}, '-=3')
-    .fromTo('.swleft', {x: -700}, {x: 0, duration: 0.5})
-    .fromTo('.swright', {x: 700}, {x: 0, duration: 0.5})
-    .fromTo('.text', {opacity: 0}, {opacity: 1, duration: 0.5})
+    .fromTo('.swleft', {x: -700}, {x: 0, duration: 1})
+    .fromTo('.swright', {x: 700}, {x: 0, duration: 1})
+    .fromTo('.text', {opacity: 0}, {opacity: 1, duration: 1.5})
 
     let scene = new ScrollMagic.Scene({
         triggerElement: 'section',
